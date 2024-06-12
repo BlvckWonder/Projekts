8d9e3c909e72a5ee08d08f1d1b2b9ba07a3f5c0b
BlvckWonder <130855752+BlvckWonder@users.noreply.github.com>
2024-06-12 17:51:17 +0000

    Canva un JS funkcijas

---
+++ script.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+  zimetuzcanva();
+}
+
+function zimetuzcanva() {
+  var kanva  = document.getElementById("zimejums");
+  var konteksts = kanva.getContext("2d");
+  konteksts.fillStyle = "green";
+  konteksts.fillRect(20,20,150,100);
+}
+function taisnsturis() {
+  var kanva  = document.getElementById("zimejums");
+  var konteksts = kanva.getContext("2d");
+  konteksts.strokeStyle = "red";
+  konteksts.strokeRect(60,60,150,100);
+}
+function aplis() {
+  var kanva  = document.getElementById("zimejums");
+  var konteksts = kanva.getContext("2d");
+  konteksts.beginPath();
+  konteksts.arc(200,200,50,0,2*Math.PI);
+  konteksts.lineWidth = 20;
+  konteksts.fillStyle = "yellow
+  konteksts.strokeStyle = "red";
+  konteksts.fill();
+  konteksts.stroke();
+}
+function linija() {
+  var kanva  = document.getElementById("zimejums");
+  var konteksts = kanva.getContext("2d");
+  konteksts.beginPath();
+  konteksts.moveTo(50,300);
+  konteksts.lineTo(250,350);
+  konteksts.lineWidth = 5;
+  konteksts.strokeStyle = "green";
+  konteksts.stroke();
+}
