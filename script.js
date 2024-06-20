4f6a7238330e990af7eb51466f0b630b40ea0b24
BlvckWonder <130855752+BlvckWonder@users.noreply.github.com>
2024-06-20 13:17:00 +0000

    Papildināts  portfolio 19.06.2024

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 window.onload = function() {
   zimetuzcanva();
+  var navLinks = document.querySelectorAll('.topnav a');
+    navLinks.forEach(function(link) {
+      link.addEventListener('click', function() {
+        navLinks.forEach(function(link) {
+          link.classList.remove('active');
+        });
+        this.classList.add('active');
+      });
+    });
+  }
+
+function atjaunotIetvaru(which) {
+  document.getElementById("lapas_saturs").innerHTML = '<'+'object id="lapas" type="text/html" data="'+which.href+'"></'+'object>';
 }
 
 function zimetuzcanva() {
@@ -35,3 +48,25 @@ function linija() {
   konteksts.strokeStyle = "green";
   konteksts.stroke();
 }
+
+function aprekins() {
+  let vards = document.getElementById("vards").value;
+  let pirmais = parseFloat(document.getElementById("a").value);
+  let otrais = parseFloat(document.getElementById("b").value);
+  if (!vards.match(/^\S[a-zA-Zā-žĀ-Ž\s]+$/)) {
+    alert("Lūdzu ievadiet derīgu vārdu!"); 
+      return;
+  }
+if (pirmais === "" || otrais === "" || isNaN(pirmais) || isNaN(otrais)){
+  alert("Ievadi skaitli");
+  return;
+}
+if (!Number. isInteger(pirmais) || !Number. isInteger(otrais)){
+  alert("Ievadi veselu skaitli");
+  return;
+}
+  
+  let summa = pirmais * otrais;
+  console.log("Tevi sauc: " + vards + " Atbilde ir:" + summa);
+  document.getElementById("rezultats").innerHTML = "Tevi sauc: " + vards + "<br>Tu ievadīji a vērtību: " + pirmais + "<br> un otrā vērtība: "+ otrais +"<br><br> Atbilde ir:" + summa;}
+
